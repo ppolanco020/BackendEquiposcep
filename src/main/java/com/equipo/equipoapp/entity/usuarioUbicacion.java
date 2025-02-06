780b47c5f1a655fec42e216a63ff10167a315c87
ppolanco020 <ppolanco020@gmail.com>
2025-02-05 21:32:38 -0600

    entity y repo
    usuario_equipo  equipo_ubicacion

---
+++ src/main/java/com/equipo/equipoapp/entity/usuarioUbicacion.java
@@ -0,0 +1,73 @@
+package com.equipo.equipoapp.entity;
+import java.io.Serializable;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="usuario_ubicacion")
+
+
+public class usuarioUbicacion implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4169381654194846293L;
+
+
+	/**
+	 * 
+	 */
+
+
+
+	@Id
+	@Column(name = "idusuarioUbicacion")
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional=false)
+	private String idusuarioubicacion;
+	
+
+	@Column(name = "ubicacionIdubicacion")
+	private String ubicacionidubicacion;
+
+	@Column(name = "usuarioEmail")
+	private String usuarioemail;
+
+	public String getIdusuarioubicacion() {
+		return idusuarioubicacion;
+	}
+
+	public void setIdusuarioubicacion(String idusuarioubicacion) {
+		this.idusuarioubicacion = idusuarioubicacion;
+	}
+
+	public String getUbicacionidubicacion() {
+		return ubicacionidubicacion;
+	}
+
+	public void setUbicacionidubicacion(String ubicacionidubicacion) {
+		this.ubicacionidubicacion = ubicacionidubicacion;
+	}
+
+	public String getUsuarioemail() {
+		return usuarioemail;
+	}
+
+	public void setUsuarioemail(String usuarioemail) {
+		this.usuarioemail = usuarioemail;
+	}
+
+	
+
+
+
+	
+}
---
+++ src/main/java/com/equipo/equipoapp/repository/usuarioUbicacionRepository.java
@@ -0,0 +1,19 @@
+package com.equipo.equipoapp.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.equipo.equipoapp.entity.usuarioUbicacion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository("usuarioUbicacionRepository")
+
+public interface usuarioUbicacionRepository extends  JpaRepository<usuarioUbicacion, String>  {
+
+
+	
+}
